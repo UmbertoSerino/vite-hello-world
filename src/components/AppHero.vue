702b71a6e31541d5488d3faeaf7fb3708b99ef55
UmbertoSerino <serumberto@gmail.com>
2023-12-13 19:49:16 +0100

    created componets AppTitle and add text and created AppHero add img with text

---
+++ src/components/AppHero.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name: 'AppHero',
+    data() {
+        return {
+
+        }
+    },
+    methods() {
+
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <img src="../assets/Sfondi-natalizi.jpg" alt="Christmas-background-image" srcset="">
+        <h1 class="title-apphero">Io sono il title nell'AppHero</h1>
+    </div>
+</template>
+
+<style scoped>
+.jumbotron {
+    position: relative;
+    width: 700px;
+    margin: 0 auto;
+}
+
+.jumbotron img {
+    width: 100%;
+}
+
+h1.title-apphero {
+    position: absolute;
+    top: 20%;
+    left: 30%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    margin: 0;
+    padding: 20px;
+    font-size: 3rem;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'AppTitle',
+  data() {
+    return {
+
+    }
+  },
+  methods() {
+
+  }
+}
+</script>
+
+<template>
+  <h1>
+    Io sono IL TITLE di AppTitle, sia fatta la mia volonta' come in frontEnd e così in BackEnd!(quasi ja)
+  </h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: lime;
+}
+
+.read-the-docs {
+  color: #888;
+}
+</style>
